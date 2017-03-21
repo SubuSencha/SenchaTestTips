93ee4000b10ec16dc2867820da45b8dadb3fdef3
SubuSencha <sububaskaran@Mac-MG8WP.local>
2017-03-21 11:50:49 -0700

    March24-Tip
    Tip1 - ST API Introduction

---
+++ TipOfTheWeek/test/MarchTips/Tip1.js
@@ -0,0 +1,20 @@
+describe("Tip1", function() {
+    it("should pass", function() {
+        //Click on the email list item
+        ST.component('treelistitem[text=Email]').click();
+        //Click on the compose menu item
+        ST.component('menuitem[text=Compose]').click();
+        //Enter a value in the text field
+        ST.textField('textfield[fieldLabel=Subject]')
+        .click()
+        .type("hello");
+        //Check if discard, save and send buttons are visible on this page
+        ST.button('button[text=Discard]').visible();
+        ST.button('button[text=Save]').enabled();
+        ST.button('button[text=Send]').enabled();
+        
+        //Click on the discard button
+        ST.button('button[text=Discard]').click();
+        
+    });
+});
