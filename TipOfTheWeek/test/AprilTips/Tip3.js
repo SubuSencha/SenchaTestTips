7f25746d1136919a5bfbe3ced49f3105af951105
SubuSencha <sububaskaran@Mac-MG8WP.local>
2017-04-10 22:52:25 -0700

    added comments
    added comments to the code

--- TipOfTheWeek/test/AprilTips/Tip3.js
+++ TipOfTheWeek/test/AprilTips/Tip3.js
@@ -2,13 +2,13 @@ describe("Simplecombobox", function() {
     
     it("should check if combobox generates the correct value", function() {
     
-        ST.comboBox('combo[reference="states"]')
-            .expand()
-            .element('boundlist => [data-recordindex="1"]')
-            .click();
+        ST.comboBox('combo[reference="states"]')//locate the combobox
+            .expand()//wait for the list to be available
+            .element('boundlist => [data-recordindex="1"]')//select using data record index
+            .click();//click on the desired value
     
         ST.field('displayfield[fieldLabel=\"Selected State\"]')
-            .value("Alaska");
+            .value("Alaska");//check if the data displayed value is as expected
 
     });
 
